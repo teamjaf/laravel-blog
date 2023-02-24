fe0321e5695e7ba18cecc08b7dbc6ff3b7f2c1e9
Md. Jafrin Hossin <kingjaaaf@gmail.com>
2023-02-24 22:09:15 +0600

    profile image updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -29,4 +29,11 @@ class AdminController extends Controller
         $adminData = User::find($id);
         return view('admin.admin_profile_view', compact('adminData'));
     }
+
+    public function EditProfile(){
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.admin_profile_edit', compact('editData'));
+
+    }
 }
