dba2712619fbfcb05761950ecf69721f7adbf3e2
Md. Jafrin Hossin <kingjaaaf@gmail.com>
2023-02-24 12:10:02 +0600

    header done

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -107,7 +107,7 @@
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-1.jpg"
+                    <img class="rounded-circle header-profile-user" src="{{'backend/assets/images/users/avatar-1.jpg'}}"
                          alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1">Julia</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
