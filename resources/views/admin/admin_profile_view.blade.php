61da5a057653a265b0121eb30f4267c0dc0ce2d4
Md. Jafrin Hossin <kingjaaaf@gmail.com>
2023-02-25 01:51:27 +0600

    view added for admin

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -9,8 +9,7 @@
                 <div class="col-lg-6">
                     <div class="card"><br><br>
                         <center>
-                            <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg')  }}" alt="Card image cap">2
-{{--                            <img class="rounded-circle avatar-xl" src="{{ (!empty($adminData->profile_image))? url('upload/admin_images/'.$adminData->profile_image):url('upload/no_image.jpg') }}" alt="Card image cap">--}}
+                            <img class="rounded-circle avatar-xl" src="{{ (!empty($adminData->profile_image))? url('upload/admin_images/'.$adminData->profile_image):url('upload/no_image.jpg') }}" alt="Card image cap">
                         </center>
 
                         <div class="card-body">
@@ -20,7 +19,7 @@
                             <hr>
                             <h4 class="card-title">User Name : {{ $adminData->username }} </h4>
                             <hr>
-                            <a href="{{route('edit.profile')}}" class="btn btn-info btn-rounded waves-effect waves-light" > Edit Profile</a>
+                            <a href="{{ route('edit.profile') }}" class="btn btn-info btn-rounded waves-effect waves-light" > Edit Profile</a>
 
                         </div>
                     </div>
@@ -32,5 +31,5 @@
 
         </div>
     </div>
-@endsection
 
+@endsection
