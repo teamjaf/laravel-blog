1de6ed6dd6fefdbf2e75e0e843e2ef8fdd60743e
Md. Jafrin Hossin <kingjaaaf@gmail.com>
2023-02-25 12:29:17 +0600

    toaster for update profile

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -4,10 +4,11 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Dashboard | Upcube - Admin & Dashboard Template</title>
+    <title>Dashboard | Easy Inventory Shop </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />
+
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
 
@@ -33,6 +34,7 @@
     <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
 </head>
 
 <body data-topbar="dark">
@@ -44,7 +46,11 @@
 
 
     @include('admin.body.header')
+
+    <!-- ========== Left Sidebar Start ========== -->
     @include('admin.body.sidebar')
+    <!-- Left Sidebar End -->
+
 
 
     <!-- ============================================================== -->
@@ -53,10 +59,9 @@
     <div class="main-content">
 
         @yield('admin')
+        <!-- End Page-content -->
+
         @include('admin.body.footer')
-
-
-
 
 
     </div>
@@ -65,37 +70,89 @@
 </div>
 <!-- END layout-wrapper -->
 
+<!-- Right Sidebar -->
+
+<!-- /Right-bar -->
 
 <!-- Right bar overlay-->
 <div class="rightbar-overlay"></div>
 
 <!-- JAVASCRIPT -->
-<script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
+<script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
 
 
 <!-- apexcharts -->
-<script src="{{asset('backend/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
 <!-- jquery.vectormap map -->
-<script src="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js')}}"></script>
+<script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
 
 <!-- Required datatable js -->
-<script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 
 <!-- Responsive examples -->
-<script src="{{asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
 
-<script src="{{asset('backend/assets/js/pages/dashboard.init.js')}}"></script>
+<script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
 
 <!-- App js -->
-<script src="{{asset('backend/assets/js/app.js')}}"></script>
+<script src="{{ asset('backend/assets/js/app.js') }}"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+    @if(Session::has('message'))
+    var type = "{{ Session::get('alert-type','info') }}"
+    switch(type){
+        case 'info':
+            toastr.info(" {{ Session::get('message') }} ");
+            break;
+
+        case 'success':
+            toastr.success(" {{ Session::get('message') }} ");
+            break;
+
+        case 'warning':
+            toastr.warning(" {{ Session::get('message') }} ");
+            break;
+
+        case 'error':
+            toastr.error(" {{ Session::get('message') }} ");
+            break;
+    }
+    @endif
+</script>
+
+<!-- Required datatable js -->
+<script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+
+<!-- Datatable init js -->
+<script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script>
+
+<script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+<script src="{{ asset('backend/assets/js/code.js') }}"></script>
+
+
+<script src="{{ asset('backend/assets/js/handlebars.js') }}"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js" ></script>
+
+<!--  For Select2 -->
+<script src="{{ asset('backend/assets/libs/select2/js/select2.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/pages/form-advanced.init.js') }}"></script>
+<!-- end  For Select2 -->
+
 </body>
 
 </html>
